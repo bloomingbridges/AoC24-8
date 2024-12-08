3033e48bd4b9c63a374652049d50933da3bca42e
bloomingbridges <brueckner.florian@googlemail.com>
2024-12-08 14:31:21 +0100

    Set up some visual debugging.

--- grid_map.py
+++ grid_map.py
@@ -1,11 +1,22 @@
+from copy import deepcopy
+
 class GridMap:
     def __init__(self, data):
         self.data = data
         self.width = len(data[0]) - 1 # Newline character
         self.height = len(data)
         self.antennae = {}
+        self.antinodes = []
+        self.grid = []
+        self.setup_grid()
         print("// New {w}x{h} GridMap created".format(w=self.width, h=self.height))
-        self.find_antennae()
+
+    def setup_grid(self):
+        for r in range(self.height):
+            row = []
+            for c, char in enumerate(self.data[r]):
+                row.append('.')
+            self.grid.append(row)
 
     def find_antennae(self):
         print("// Tuning in..")
@@ -21,3 +32,32 @@ class GridMap:
             self.antennae[frequency].append((x, y))
         else:
             self.antennae[frequency] = [(x, y)]
+
+    def project_antinodes(self):
+        pass
+
+    def print_debug_maps(self):
+        antinode_map = deepcopy(self.grid)
+        self.draw_map(antinode_map, "Antinodes only")
+
+        antennae_map = deepcopy(self.grid)
+        for f, frequency in enumerate(self.antennae):
+            for c, coords in enumerate(self.antennae[frequency]):
+                antennae_map[coords[1]][coords[0]] = frequency
+        self.draw_map(antennae_map, "Antennae only")
+
+        # reference_map = deepcopy(self.grid)
+        # self.draw_map(reference_map, "Reference input")
+
+        composite_map = deepcopy(self.grid)
+        self.draw_map(composite_map)
+
+    def draw_map(self, map, name = "Composite output"):
+        print("// " + name)
+        for y, row in enumerate(map):
+            row_string = ""
+            for x, col in enumerate(row):
+                if (x < self.width):
+                    row_string += col
+            print(row_string)
+        print()
